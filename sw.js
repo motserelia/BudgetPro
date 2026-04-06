bd37be010779e48cb7891e6df7d28ce5f6edce1c
motserelia <121040322+motserelia@users.noreply.github.com>
2026-04-06 19:29:15 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,28 @@
-const CACHE_NAME = 'budgetpro-v1';
+const CACHE_NAME = "budgetpro-v1";
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/assets/css/index.css',
-  '/assets/js/index.js',
-  '/assets/images/money.jpg'
+  "/",
+  "/index.html",
+  "/index.css",
+  "/index.js",
+  "/i18n.js",
+  "/manifest.json",
+  "/money.jpg",
+  "/favicon-96x96.png",
+  "/favicon.svg",
+  "/favicon.ico",
+  "/apple-touch-icon.png",
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)),
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches
+      .match(event.request)
+      .then((response) => response || fetch(event.request)),
   );
 });
