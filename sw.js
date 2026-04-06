41ed2d88dfec4ec050cea1a4989852e7eae41ba8
motserelia <121040322+motserelia@users.noreply.github.com>
2026-04-06 19:25:15 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,22 @@
 const CACHE_NAME = 'budgetpro-v1';
 const urlsToCache = [
-  '/BudgetPro/',
-  '/BudgetPro/index.html',
-  '/BudgetPro/assets/css/index.css',
-  '/BudgetPro/assets/js/index.js',
-  '/BudgetPro/assets/images/money.jpg'
+  '/',
+  '/index.html',
+  '/assets/css/index.css',
+  '/assets/js/index.js',
+  '/assets/images/money.jpg'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
