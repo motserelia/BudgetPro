a7802fa3290a926b0c99a43beebae09a24e96388
motserelia <121040322+motserelia@users.noreply.github.com>
2026-04-06 20:21:24 +0400

    Update sw.js

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -1,10 +1,11 @@
-const CACHE_NAME = "budgetpro-v1";
+const CACHE_NAME = "budgetpro-v2"; // новая версия
 const urlsToCache = [
   "/",
   "/index.html",
   "/index.css",
   "/index.js",
   "/i18n.js",
+  "/locales.js",   // ← добавлен locales.js
   "/manifest.json",
   "/money.jpg",
   "/favicon-96x96.png",
